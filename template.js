f1822514184aa12c23354721f52785b1879ab76d
briverse17 <vumnguyenn@gmail.com>
2020-09-16 17:54:13 +0700

    add template

---
+++ template.js
@@ -0,0 +1,76 @@
+/**
+ * DATA STRUCTURES AND ALGORITHMS IMPLEMENTATION IN JAVASCRIPT
+ * <Data Structures / Algorithms to be implemented in the file>
+ *  - Searching Algorithm 1
+ *  - Searching Algorithm 2
+ *  ...
+ *  - Searching Algorithm N
+*/
+
+// Packages declaration (if needed)
+// ================================================
+// Your code here
+// ================================================
+
+// Toy data decleration (if needed)
+// ================================================
+// Your code here
+// ================================================
+
+// Functions definition
+// ================================================
+/**
+ * Decsription
+ * of
+ * function1
+ * @param {datatype} param1 param1 description
+ * @param {datatype} param2 param2 description
+ * ...
+ * @param {datatype} paramN paramN description
+ * @returns {datatype} returnValue - return value description
+ */
+function function1(param1, param2, paramN) {
+    // Your code here
+}
+
+/**
+ * Decsription
+ * of
+ * function2
+ * @param {datatype} param1 param1 description
+ * @param {datatype} param2 param2 description
+ * ...
+ * @param {datatype} paramN paramN description
+ * @returns {datatype} returnValue - return value description
+ */
+function function2(param1, param2, paramN) {
+    // Your code here
+}
+
+// ...
+
+/**
+ * Decsription
+ * of
+ * functionN
+ * @param {datatype} param1 param1 description
+ * @param {datatype} param2 param2 description
+ * ...
+ * @param {datatype} paramN paramN description
+ * @returns {datatype} returnValue - return value description
+ */
+function functionN(param1, param2, paramN) {
+    // Your code here
+}
+// ================================================
+
+// Driver code (if needed)
+// ================================================
+// Your code here
+// ================================================
+
+/**
+ * Authors: Nhut-Nam Le & Minh-Vu Nguyen
+ * VNU-HCM, University of Science
+ * © 2020
+ */
