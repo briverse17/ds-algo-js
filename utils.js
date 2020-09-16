8c3b5780575d471e852f8a6a13ed55860c9b4b78
briverse17 <vumnguyenn@gmail.com>
2020-09-16 17:54:45 +0700

    add utils: sleep

---
+++ utils.js
@@ -0,0 +1,9 @@
+module.exports = {
+    /**
+     * Make the console sleep for ms miliseconds
+     * @param {number} ms the duration of time to sleep
+     */
+    sleep: function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    },
+}
