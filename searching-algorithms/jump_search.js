d03385240b977f6e0c11a2bbeb43a9b4928ea13a
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-17 14:55:49 +0700

    add .prototype function

--- searching-algorithms/jump_search.js
+++ searching-algorithms/jump_search.js
@@ -1,14 +1,19 @@
-let arr = [
-    1, 5, 7, 10, 12, 14, 15,
-    18, 20, 22, 25, 27, 30,
-    64, 110, 220];
+/**
+ * DATA STRUCTURES AND ALGORITHMS IMPLEMENTATION IN JAVASCRIPT
+ * Exponential search algorithm
+*/
 
+/**
+ * 
+ * @param {any} array 
+ * @param {any} value 
+ */
 let jump_search = (array, value) => {
     let step = Math.floor(Math.sqrt(array.length));
 
     let lower = 0;
 
-    while (array[Math.min(step, array.length) -1] < value) {
+    while (array[Math.min(step, array.length) - 1] < value) {
         lower = step;
         step = -~step;
 
@@ -17,11 +22,12 @@ let jump_search = (array, value) => {
         }
     }
 
-    let upper = Math.min(step, array.length) 
-    while(array[lower] < value) {
+    let upper = Math.min(step, array.length);
+
+    while (array[lower] < value) {
         lower++;
 
-        if (lower === upper){
+        if (lower === upper) {
             return -1;
         }
 
@@ -34,4 +40,50 @@ let jump_search = (array, value) => {
     return -1;
 };
 
-console.log(jump_search(arr, 30));
+/**
+ * Add method isSorted to array prototype
+ * @returns true if array is sorted and false for the opposite case
+ */
+Array.prototype.is_sorted = function () {
+    const length = this.length;
+    for (let i = 0; i < length - 1; i++) {
+        if (this[i] > this[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+Array.prototype.jump_searching = function (value) {
+
+    // check array is sorted?
+    if (this.is_sorted() == true){
+        return jump_search(this, value);
+    } else {
+        console.log(this.is_sorted());
+        this.sort();
+        console.log("fuck js");
+        return jump_search(this, value);
+    }
+};
+
+// ================================================
+// Modules export
+// ================================================
+module.exports = {
+    JumpSearch: jump_search
+};
+
+// Driver code (if needed)
+// ================================================
+const run = async () => {
+
+};
+
+// ================================================
+/**
+ * Authors: Nhut-Nam Le & Minh-Vu Nguyen
+ * VNU-HCM, University of Science
+ * © 2020
+ */
+
