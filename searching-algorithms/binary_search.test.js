0f78762219faac16cd39fb0cbce96d4c293e768e
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-17 12:34:00 +0700

    add test for recursive binary

--- searching-algorithms/binary_search.test.js
+++ searching-algorithms/binary_search.test.js
@@ -2,11 +2,28 @@ const search = require('./binary_search');
 
 let binary_search = search.BinarySearch;
 
-let test_arr = [
-    1, 5, 7, 10, 12, 14, 15,
-    18, 20, 22, 25, 27, 30,
-    64, 110, 220];
+let recurive_binary_search = search.RecursiveBinarySearch;
 
 test('Testing for binary search using iterative approach', () => {
+    let test_arr = [
+        1, 5, 7, 10, 12, 14, 15,
+        18, 20, 22, 25, 27, 30,
+        64, 110, 220];
+
     expect(binary_search(test_arr, 0, test_arr.length - 1, 30)).toBe(12);
 });
+
+test('Testing for binary search using recursive approach', () => {
+    let test_arr = [
+        1, 5, 7, 10, 12, 14, 15,
+        18, 20, 22, 25, 27, 30,
+        64, 110, 220];
+
+    expect(recurive_binary_search(test_arr, 0, test_arr.length - 1, 30)).toBe(12);
+});
+
+
+// ================================================
+// Authors: Nhut-Nam Le & Minh-Vu Nguyen
+// VNU-HCM, University of Science
+// © 2020
